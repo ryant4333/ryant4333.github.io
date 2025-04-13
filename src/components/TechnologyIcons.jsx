1c6a286742d251c24988fb4c0ed961a23e419c56
Ryan Taylor <32863538+ryant4333@users.noreply.github.com>
2025-04-13 19:40:14 +1000

    more icons

--- src/components/TechnologyIcons.jsx
+++ src/components/TechnologyIcons.jsx
@@ -70,7 +70,99 @@ const iconMap = {
     src: '/icons/mongodb.svg',
     alt: 'MongoDB'
   },
-  
+  // Adding Azure icons
+  azureblob: {
+    src: '/icons/az_blob.svg',
+    alt: 'Azure Blob Storage'
+  },
+  azureaisearch: {
+    src: '/icons/az_aisearch.svg',
+    alt: 'Azure AI Search'
+  },
+  azureaistudio: {
+    src: '/icons/az_aistudio.svg',
+    alt: 'Azure AI Studio'
+  },
+  azuredocunentintelligence: {
+    src: '/icons/az_docint.svg',
+    alt: 'Azure Document Intelligence'
+  },
+  azureappinsights: {
+    src: '/icons/az_appinsight.svg',
+    alt: 'Azure Application Insights'
+  },
+  azureappservice: {
+    src: '/icons/az_appservice.svg',
+    alt: 'Azure App Service'
+  },
+  azurecontainerregistry: {
+    src: '/icons/az_contreg.svg',
+    alt: 'Azure Container Registry'
+  },
+  azurecosmos: {
+    src: '/icons/az_cosmos.svg',
+    alt: 'Azure Cosmos DB'
+  },
+  azurefunctions: {
+    src: '/icons/az_funcapp.svg',
+    alt: 'Azure Functions'
+  },
+  azureiothub: {
+    src: '/icons/az_iothub.svg',
+    alt: 'Azure IoT Hub'
+  },
+  azurekubernetes: {
+    src: '/icons/az_kubeserv.svg',
+    alt: 'Azure Kubernetes Service'
+  },
+  azurelogicapps: {
+    src: '/icons/az_logicapp.svg',
+    alt: 'Azure Logic Apps'
+  },
+  azureopenai: {
+    src: '/icons/az_openai.svg',
+    alt: 'Azure OpenAI'
+  },
+  azureservicebus: {
+    src: '/icons/az_servbus.svg',
+    alt: 'Azure Service Bus'
+  },
+  datafactory: {
+    src: '/icons/datafactory.svg',
+    alt: 'Azure Data Factory'
+  },
+  eventhub: {
+    src: '/icons/eventhub.svg',
+    alt: 'Azure Event Hub'
+  },
+  grafana: {
+    src: '/icons/grafana.svg',
+    alt: 'Grafana'
+  },
+  powerbi: {
+    src: '/icons/powerbi.svg',
+    alt: 'Power BI'
+  },
+  powerapps: {
+    src: '/icons/powerapp.svg',
+    alt: 'Power Apps'
+  },
+  sql: {
+    src: '/icons/sql.svg',
+    alt: 'SQL'
+  },
+  streamanalytics: {
+    src: '/icons/stream_analytics.svg',
+    alt: 'Azure Stream Analytics'
+  },
+  deltatable: {
+    src: '/icons/deltatable.svg',
+    alt: 'Delta Table'
+  },
+  semantickernel: {
+    src: '/icons/az_sk.png',
+    alt: 'Semantic Kernel'
+  },
   // Add more technologies as needed
 };
 
